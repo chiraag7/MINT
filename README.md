fa2844ab47a6532c02279acb157458f63fb152ac
chiraag7 <chiraag7@gmail.com>
2019-01-28 16:59:13 -0800

    Updated README.md, made scipt configurable from command line, validated input formats

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MINT
+
+  We are generating a standard layout for the given resource in the dataset.
+
+  A layout defines the physical relationship between the variables in the given resource.
+
+  We are trying to automatically generate a stnadard layout for the inputted resource instead of manually creating them.
+
+  Currently based on the combined heading of row and column in the first cell of CSV file, we are segregating them and describing the layout.
+
+### Requirements
+
+  Python3
+
+### Procedure
+
+  Run the program as follows: -
+
+    python generateLayout.py -i <input_file> -o <output_file>
+
+    <input_file> - resource file in CSV format
+    <output_file> - JSON object of the layout (optional - displays on the std output if not given)
--- generateLayout.py
+++ generateLayout.py
@@ -1,27 +1,77 @@
 import csv
+import sys
+import getopt
+import json
 
+def usage():	
+	print('Usage: python generateLayout.py -i <input_file> [ -o <output_file>]')
+	print('<input_file> must be in CSV file format.')
+	print('<ouput_file> (optional) contains a JSON object of the layout.')
+	
 
-if __name__ == '__main__':	
+def main(argv):
+	inputFile = ''
+	outputFile = ''
 
 	columnHead = ''
 	rowHead = ''
 
-	with open('Dataset/Crops_TrendAnalysis_2014-2016.csv', 'r') as csvfile:
+	try:
+		options, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print('Incorrect usage')
+		usage()
+		sys.exit(2)		
 
-		reader = csv.reader(csvfile, delimiter = ',')
+	for option, arg in options:
+		if option == 'h':
+			usage()
+			sys.exit()
+		if option in ("-i", "--ifile"):
+			inputFile = arg
+		elif option in ("-o", "--ofile"):
+			outputFile = arg
+
+	if inputFile == '':
+		print('Please enter input file in CSV file format')
+		usage()
+		sys.exit(2)
+
+	with open(inputFile, 'r') as csvfile:
+		try:
+			reader = csv.reader(csvfile, delimiter = ',')
+		except Exception:
+			print(inputFile + ' not in CSV file format.')
+
+		layoutDic = {"layout":{}}
 
 		for index, row in enumerate(reader):
 
 			if index == 0: #First row
 				rowCol = row[index].split('/')
+
 				if len(rowCol) == 2:
 					rowHead = rowCol[0].strip()
 					columnHead = rowCol[1].strip()
-					print("layout")
-					print("\t"+rowHead)
-					print("\t\tlocation: " + "1:1...")
-					print("\t"+columnHead)
-					print("\t\tlocation: " +  "1...:1")
-					print("\tresult")
-					print("\t\tlocation: 1...:1...")
+					layoutDic[rowHead] = {"location": "1:1..."}
+					layoutDic[columnHead] = {"location": "1...:1"}
+					layoutDic["result"] = {"location": "1...:1..."}
 
+				else:
+					print("Input file is not in the required format.")
+					sys.exit(1)
+
+		jsonOut = json.dumps(layoutDic, indent = 4)
+
+		if outputFile == '':
+			print(jsonOut)
+			return
+
+		with open(outputFile, 'w') as writer:
+			writer.write(jsonOut)
+			writer.close()
+
+		csvfile.close()
+					
+if __name__ == '__main__':
+	main(sys.argv[1:])
