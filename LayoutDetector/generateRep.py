72089a839ddf06d1acf83d18d094ee3e820f4653
Apoorva K H <apoorva.khp@gmail.com>
2019-04-09 16:58:55 -0700

    updating yaml representation

--- LayoutDetector/generateRep.py
+++ LayoutDetector/generateRep.py
@@ -6,6 +6,29 @@ import yaml
 """ Creates a yaml file representation using the blocks detected from BlockDetector 
 and relations detected from LayoutDetector"""
 
+def getDotNotation(cellLocations):
+    mini = cellLocations[0][0]
+    minj = cellLocations[0][1]
+    samei = True
+    samej = True
+    previ, prevj = mini, minj
+    for i,j in cellLocations[1:]:
+        if i < mini:
+            mini = i
+        if j < minj:
+            minj = j
+        if prevj != j:
+            samej = False
+        if previ != i:
+            samei = False
+    string = str(mini)
+    if not samei:
+        string += ".."
+    string += ":" + str(minj)
+    if not samej:
+        string += ".."
+    return string
+
 def generateRepresentaion():
     detectedBlocks = getBlocks("data/LivestockLoss_Cattle_Warrap_2017.csv")
     detectedRelations = getRelations("data/LivestockLoss_Cattle_Warrap_2017.csv", "data/Livestock_Loss.yaml")
@@ -14,7 +37,7 @@ def generateRepresentaion():
                 "version": "1",
                 "resources":"<r_name>",
                 "transformation": [],
-                "layout": [],
+                "layout": {},
                 "mappings": [],
                 "semantic_model":{
                     "semantic_types":[],
@@ -23,7 +46,8 @@ def generateRepresentaion():
                     }
                 }
     for block in detectedBlocks:
-        layoutJson["layout"][block.name] = "0:1.."
+        layoutJson["layout"][block.name] = {}
+        layoutJson["layout"][block.name]["location"] = getDotNotation(block.cellLocations)
     #print("detectedRelations : ", detectedRelations)
     for relation in detectedRelations:
         if relation[0] and relation[1]:
@@ -42,25 +66,4 @@ with open('representaion.yml', 'w') as output:
     yaml.dump(representaion, output, default_flow_style=False)
 
 
-def getDotNotation(cellLocations):
-    mini = cellLocations[0][0]
-    minj = cellLocations[0][1]
-    samei = True
-    samej = True
-    previ, prevj = mini, minj
-    for i,j in cellLocations[1:]:
-        if i < mini:
-            mini = i
-        if j < minj:
-            minj = j
-        if prevj != j:
-            samej = False
-        if previ != i:
-            samei = False
-    string = str(mini)
-    if !samei:
-        string += ".."
-    string += ":" + str(minij)
-    if !samej:
-        string += ".."
-    return string
+
