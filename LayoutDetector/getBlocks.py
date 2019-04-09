294effdebc944bf6a9402ffce93d88906b7ff48d
chiraag7 <chiraag7@gmail.com>
2019-04-08 20:11:03 -0700

    Updated get_blocks code

--- LayoutDetector/getBlocks.py
+++ LayoutDetector/getBlocks.py
@@ -1,9 +1,39 @@
 
+'''
+Generates blocks based on the cell position
+(Currently works for 2D resource files)
+
+@param inputFile: Input CSV file for which YAML 
+				  representation must be generated.
+
+'''
 
 
-""" Gets the blocks from BlockDetector. """
-""" This file integrates BlockDetector and LayoutDetector. """
+def getBlocks(inputFile):
+	metadata = []
+	data = []
+	column_header = []
+	row_header = []
 
+    with open(resourceFile, "r") as csvf:
 
-def getBlocks():
-    pass
+		rows = csv.reader(csvf, delimiter = ',')
+		
+		for i, row in enumerate(rows):			
+			for j, col in enumerate(row):
+
+				if col.strip() != '':
+					if i==0 and j==0:
+						metadata.append((i,j))
+					elif i==0:
+						column_header.append((i,j))
+					elif j==0:
+						row_header.append((i,j))
+					else:
+						data.append((i,j))
+
+	metaDataBlock = Block(metadata, 3, "metadata")
+	dataBlock = Block(data, 0, "data")
+	colHeaderBlock = Block(column_header, 1, "column_header")
+	rowHeaderBlock = Block(row_header, 2, "row_header")
+	allBlocks = [metaDataBlock, dataBlock, colHeaderBlock, rowHeaderBlock]
