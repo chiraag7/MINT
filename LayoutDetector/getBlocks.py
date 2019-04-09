9e68f11d798ea3639850d45f97823107c7282fff
Apoorva K H <apoorva.khp@gmail.com>
2019-04-09 14:20:56 -0700

    resolving merge conflicts

--- LayoutDetector/getBlocks.py
+++ LayoutDetector/getBlocks.py
@@ -10,24 +10,11 @@ Generates blocks based on the cell position
 import csv
 from LayoutDetector.block import *
 
-=======
-'''
-Generates blocks based on the cell position
-(Currently works for 2D resource files)
-
-@param inputFile: Input CSV file for which YAML 
-				  representation must be generated.
-
-'''
->>>>>>> 294effdebc944bf6a9402ffce93d88906b7ff48d
-
 def getBlocks(inputFile):
     metadata = []
     data = []
     column_header = []
     row_header = []
-
-<<<<<<< HEAD
 
     with open(inputFile, "r") as csvf:
         rows = csv.reader(csvf, delimiter=',')
@@ -51,33 +38,3 @@ def getBlocks(inputFile):
     rowHeaderBlock = Block(row_header, 2, "row_header")
     allBlocks = [metaDataBlock, dataBlock, colHeaderBlock, rowHeaderBlock]
     return allBlocks
-=======
-def getBlocks(inputFile):
-	metadata = []
-	data = []
-	column_header = []
-	row_header = []
-
-    with open(resourceFile, "r") as csvf:
-
-		rows = csv.reader(csvf, delimiter = ',')
-		
-		for i, row in enumerate(rows):			
-			for j, col in enumerate(row):
-
-				if col.strip() != '':
-					if i==0 and j==0:
-						metadata.append((i,j))
-					elif i==0:
-						column_header.append((i,j))
-					elif j==0:
-						row_header.append((i,j))
-					else:
-						data.append((i,j))
-
-	metaDataBlock = Block(metadata, 3, "metadata")
-	dataBlock = Block(data, 0, "data")
-	colHeaderBlock = Block(column_header, 1, "column_header")
-	rowHeaderBlock = Block(row_header, 2, "row_header")
-	allBlocks = [metaDataBlock, dataBlock, colHeaderBlock, rowHeaderBlock]
->>>>>>> 294effdebc944bf6a9402ffce93d88906b7ff48d
